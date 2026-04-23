c869748d02e75a2647cf11d0caa522755e4114e9
Mohamed Alteneiji <your-github-email@example.com>
2026-04-23 11:27:35 +0400

    refine: remove prices from merch cards for pre-launch state

--- scripts/merch.js
+++ scripts/merch.js
@@ -2,7 +2,7 @@ export async function initMerch(){
 const data=await fetch('./data/merch.json').then(r=>r.json());
 const cards=await Promise.all(data.map(async item=>{
   const svgText=await fetch('./components/merch-svgs/'+item.svg).then(r=>r.text());
-  return '<div class="mc rv '+item.revealClass+'"><div class="mc-img">'+svgText+'<div class="mc-layer">Coming Soon</div></div><div class="mc-name">'+item.name+'</div><div class="mc-price"><span class="mc-price-num">'+item.priceAed+'</span><span class="mc-price-unit">AED</span></div></div>';
+  return '<div class="mc rv '+item.revealClass+'"><div class="mc-img">'+svgText+'<div class="mc-layer">Coming Soon</div></div><div class="mc-name">'+item.name+'</div></div>';
 }));
 document.querySelector('.merch-grid').innerHTML=cards.join('');
 }
